ae1fb5838aac4b5359dd69d996ad6678041ee440
angelmidnighttt <thongcaovan080598@gmail.com>
2026-05-24 18:31:56 +0700

    refactor

--- Storage/LogRecord.cs
+++
@@ -1,9 +0,0 @@
-namespace MyDb.Storage;
-
-public sealed class LogRecord
-{
-    public required string Key { get; init; }
-    public required byte[] Value { get; init; }
-    public bool IsTombstone { get; init; }
-    public long Offset { get; init; }
-}
